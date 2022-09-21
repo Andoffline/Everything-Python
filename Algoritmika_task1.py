653fef2960e1d982c4ce4f8db31dddf171f129fe
AndOffline <pogodaev.and@gmail.com>
2022-09-21 17:15:20 +0300

    Task "Salary bonus"

---
+++ Algoritmika_task1.py
@@ -0,0 +1,4 @@
+# Task to determine salary bonus, by finding percent using monthly salary and weekend hours
+ZP=float(input('Monthly salary:'))
+Time=float(input('Number of weekend hours:'))
+print('Salary bonus:', ZP*0.01*Time)
