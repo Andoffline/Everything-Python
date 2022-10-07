7c09bc18443c1bc9872b8b3768e5adb8ed9bd7a7
AndOffline <pogodaev.and@gmail.com>
2022-10-07 13:23:04 +0300

    Homework 1 Data Programming

---
+++ DP_Homework1.py
@@ -0,0 +1,19 @@
+classroom = ['john', 'maria', 'maria', 'ahmet', 'eleni', 'rho', 'john', 'maria', 'jules']
+#Function used to count number of times name is mentioned in the list
+def count(list,name):
+    count=0
+    i=0
+    while i in range(0,len(list)):
+        if list[i]==name:
+            count+=1
+            i+=1
+        else:
+            i+=1
+    print('Number of times name ',name,'memtioned is: ',count)
+
+name=input('Enter name of student:')
+count(classroom,name)
+
+    
+
+    
