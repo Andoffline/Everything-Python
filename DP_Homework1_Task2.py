4c0b404c0441e5420c21371765e7d92aa267a1a3
AndOffline <pogodaev.and@gmail.com>
2022-10-14 21:57:59 +0300

    Created isStrong function. Started the creation of findStrong function

--- DP_Homework1_Task2.py
+++ DP_Homework1_Task2.py
@@ -1,10 +1,35 @@
 #Exercise2
 import math
+num=int(input())
 def isStrong(num):
-    if num==(math.factorial(num%10000))+(math.factorial(num%1000))+(math.factorial(num%100))+(math.factorial(num%10)):
+    num1=num
+    sum=0
+    while num!=0:
+        temp=num%10
+        sum+=math.factorial(temp)
+        num=num//10
+        temp=0
+
+    if num1==sum:
         return True
     else:
         return False
 
+print(isStrong(num))
 
+def findStrongs(tuple):
+    list=[]
+    j=1
+    i=0
+    for i in range(tuple[0],tuple[1]):
+        if(isStrong(i)==True):
+            list[j]=i
+            j+=1
+        i+=1
+    return list
+
+range_s=int(input('Enter starting range:'))
+range_e=int(input('Enter ending range:'))
+tuple(range_s,range_e)
+print(findStrongs(tuple(0),tuple(1)))
     
